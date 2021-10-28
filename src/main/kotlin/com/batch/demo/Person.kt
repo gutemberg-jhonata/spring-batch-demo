fc96a8d404d4f5df5277889de56034ef1e60b2d5
Jhonata <gutemberg.jhonata@gmail.com>
2021-10-28 09:25:12 -0300

    Refactor Person with kotlin

--- src/main/kotlin/com/batch/demo/Person.kt
+++ src/main/kotlin/com/batch/demo/Person.kt
@@ -1,4 +1,16 @@
 package com.batch.demo
 
 class Person {
+    lateinit var firstName: String
+    lateinit var lastName: String
+
+    constructor() {}
+    constructor(firstName: String, lastName: String) {
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+
+    override fun toString(): String {
+        return "firstName: $firstName, lastName: $lastName"
+    }
 }
