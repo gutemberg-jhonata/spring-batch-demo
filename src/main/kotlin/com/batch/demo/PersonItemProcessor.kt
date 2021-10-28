86fca11b45b35f5dae3ccbf220ac046690b1418c
Jhonata <gutemberg.jhonata@gmail.com>
2021-10-28 08:54:40 -0300

    Refactor PersonItemProcessor to kotlin

---
+++ src/main/kotlin/com/batch/demo/PersonItemProcessor.kt
@@ -0,0 +1,22 @@
+package com.batch.demo
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.batch.item.ItemProcessor
+
+class PersonItemProcessor : ItemProcessor<Person, Person> {
+
+    private val log: Logger = LoggerFactory.getLogger(PersonItemProcessor::class.java)
+
+    override fun process(person: Person): Person? {
+        val firstName = person.firstName.uppercase()
+        val lastName = person.lastName.uppercase()
+
+        val transformedPerson = Person(firstName, lastName)
+
+        log.info("Converting ($person) into ($transformedPerson)")
+
+        return transformedPerson
+    }
+
+}
